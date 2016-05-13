7e140243155199d4dc013d8c97ec71a95d552fc7
Randall Britten <randall.britten@gmail.com>
2016-05-14 10:06:09 +1200

    Update Player.hs

--- Player.hs
+++ Player.hs
@@ -7,7 +7,7 @@ version :: String
 version = "Default Haskell folding player"
 
 betRequest :: Object -> IO Int
-betRequest gameState = return 10000
+betRequest gameState = return 0
 
 showdown :: Object -> IO ()
 showdown gameState = return ()
