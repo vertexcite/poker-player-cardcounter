8061f97d8533484d267e44c49d661294c8e34d9c
Randall Britten <randall@movio.co>
2016-05-14 16:12:36 +1200

    Try something different

--- Player.hs
+++ Player.hs
@@ -1,13 +1,19 @@
 module Player where
 
 import Data.Aeson (Object)
-
+import System.Random
 
 version :: String
-version = "Default Haskell folding player"
+version = "Do something, anything"
 
 betRequest :: Object -> IO Int
-betRequest gameState = return 0
+betRequest gameState = do
+  value <- randomIO
+  let
+    bet :: Int
+    bet = value `mod` 100
+    reply = if abs bet < 20 then 100000 else 0
+  return reply
 
 showdown :: Object -> IO ()
 showdown gameState = return ()
